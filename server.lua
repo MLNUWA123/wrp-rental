7301cd49fa59b58752e7e1a710ade0d9fbb30e29
MLNUWA123 <69652069+MLNUWA123@users.noreply.github.com>
2022-05-15 21:01:00 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -6,10 +6,24 @@ TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 RegisterServerEvent('esx_rental:rent')
 AddEventHandler('esx_rental:rent', function()
     local xPlayer = ESX.GetPlayerFromId(source)
+    local whatever = xPlayer.getMoney - Config.Price
 
     if xPlayer.getMoney => Config.Price then 
         xPlayer.removeMoney(Config.Price)
     else 
-        TriggerClientEvent('pNotify:SendNotification', source, 'Nie posiadasz wystarczająco gotówki')
+        TriggerClientEvent('pNotify:SendNotification', source, 'Brakuje Ci: $' ..whatever)
+    end
+end)
+
+
+RegisterServerEvent('esx_rental:rentboat')
+AddEventHandler('esx_rental:rentboat', function()
+        local xPlayer = ESX.GetPlayerFromId(source) 
+        local whatever = xPlayer.getMoney - Config.Boats
+        
+      if xPlayer.getMoney => Config.Boats then 
+        xPlayer.removeMoney(Config.Boats)
+    else 
+        TriggerClientEvent('pNotify:SendNotification', source, 'Brakuje Ci: $' ..whatever)
     end
 end)
